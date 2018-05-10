f028a422ccf3cedc8cd7717c68eca52bf2f48078
Vahram <vahram.shaboyan.g@tumo.org>
2018-05-10 14:22:22 +0400

    Files are added.

--- class.hunter.js
+++ class.hunter.js
@@ -1,44 +1,13 @@
-class Hunter {
+class Hunter extends LivingCreatures{
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.index = index;
+        super(x, y, index);
         this.energy = 30;
-    }
-
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-
-
-    chooseCell(character) {
-        this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
     }
 
     getNewLargerCoordinates() {
         this.directions = [
             [this.x - 1, this.y - 2],
-            [this.x, this.y - 2],
+            [this.x,     this.y - 2],
             [this.x + 1, this.y - 2],
             [this.x + 2, this.y - 2],
             [this.x - 2, this.y - 1],
@@ -58,7 +27,7 @@ class Hunter {
             [this.x + 2, this.y + 1],
             [this.x - 2, this.y + 2],
             [this.x - 1, this.y + 2],
-            [this.x, this.y + 2],
+            [this.x,     this.y + 2],
             [this.x + 1, this.y + 2],
             [this.x + 2, this.y + 2],
         ];
--- class.livingcreatures.js
+++ class.livingcreatures.js
@@ -1,9 +1,10 @@
-class Grass {
-    constructor(x, y, index) {
+class LivingCreatures {
+    constructor (x, y, index){
         this.x = x;
         this.y = y;
         this.index = index;
-        this.multiply = 0;
+    }
+    getNewCoordinates() {
         this.directions = [
             [this.x - 1, this.y - 1],
             [this.x,     this.y - 1],
@@ -15,8 +16,8 @@ class Grass {
             [this.x + 1, this.y + 1]
         ];
     }
-
     chooseCell(character) {
+        this.getNewCoordinates();
         var found = [];
         for (var i in this.directions) {
             var x = this.directions[i][0];
@@ -29,21 +30,4 @@ class Grass {
         }
         return found;
     }
-    mul() {
-        this.multiply++;
-        var emptyCells = this.chooseCell(0);
-        var newCell = random(emptyCells);
-
-
-        if (newCell && this.multiply >= 3) {
-            var newX = newCell[0];
-            var newY = newCell[1];
-            matrix[newY][newX] = this.index;
-
-            var newGrass = new Grass(newX, newY, this.index);
-            grassArr.push(newGrass);
-            this.multiply = 0;
-        }
-    }
-
 }
